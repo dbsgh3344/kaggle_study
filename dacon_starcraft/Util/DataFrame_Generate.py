a441a06d5ba05d7a7f299aecdede3335e9b2990a
songyeoneo <dbsgh3344@naver.com>
2020-04-05 14:39:24 +0900

    update EDA ability nb

--- dacon_starcraft/Util/DataFrame_Generate.py
+++ dacon_starcraft/Util/DataFrame_Generate.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 def df_counting(p0_data,p1_data,x_data,variable_name):
 
     p0_df = pd.DataFrame(p0_data.game_id.value_counts().keys(),columns=['game_id'])
@@ -12,16 +14,16 @@ def df_counting(p0_data,p1_data,x_data,variable_name):
     df.index = df.game_id
     df.drop('game_id',axis=1,inplace=True)
 
-    p0_df['P0'+variable_name] = p0.game_id.value_counts().values
-    p1_df['P1'+variable_name] = p1.game_id.value_counts().values
+    p0_df['P0_'+variable_name] = p0_data.game_id.value_counts().values
+    p1_df['P1_'+variable_name] = p1_data.game_id.value_counts().values
 
     df = pd.merge(df,p0_df,on='game_id',how='left')
     df = pd.merge(df,p1_df,on='game_id',how='left')
 
     df = df.fillna(0)
 
-    x_data['P0'+variable_name] = df['P0'+variable_name]
-    x_data['P1'+variable_name] = df['P1'+variable_name]
-    x_data['delta'+variable_name] = x_data['P0'+variable_name] - x_data['P1'+variable_name]
+    x_data['P0_'+variable_name] = df['P0_'+variable_name]
+    x_data['P1_'+variable_name] = df['P1_'+variable_name]
+    x_data['delta_'+variable_name] = x_data['P0_'+variable_name] - x_data['P1_'+variable_name]
 
     return x_data
